6ed600c01cb4a2beb064216ea5871cae5ebd44a6
edikskrim <edik.shark@mail.ru>
2018-08-22 11:33:48 +0700

    creating a block with a light bulb

--- js/gameButtonPlay.js
+++ js/gameButtonPlay.js
@@ -1,5 +1,5 @@
-function viewDiv(){
-			  		document.getElementById("phaser-demo").style.display = "block";
-			  		document.getElementById("content2").style.display = "none";
-			  		document.getElementById("playGame").style.display = "none";
-				};
+function viewDiv() {
+    document.getElementById("phaser-demo").style.display = "block";
+    document.getElementById("content2").style.display = "none";
+    document.getElementById("playGame").style.display = "none";
+};
